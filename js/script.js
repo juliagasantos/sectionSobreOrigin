b84adc3f1d23c8ec89adeb10fa44d8eaf4a5e80f
juliagasantos <juliagalvaoalmeidasantos@gmail.com>
2025-03-20 22:13:33 -0300

    inicio do media no css

--- js/script.js
+++ js/script.js
@@ -1,8 +1,7 @@
-function openCity(topicName) {
-  var i;
-  var x = document.getElementsByClassName("topic");
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = "none";
-  }
-  document.getElementById(topicName).style.display = "block";
+
+function showTab(tabId) {
+  document.querySelectorAll('.tab-content').forEach(el => el.classList.remove('active'));
+  document.querySelectorAll('.tab').forEach(el => el.classList.remove('active'));
+  document.getElementById(tabId).classList.add('active');
+  document.querySelector(`.tab[onclick="showTab('${tabId}')"]`).classList.add('active');
 }
